a915ab57383a8453a2a9b8fc2608fe4805363b91
VMatos <veronica.matos@vision-box.com>
2023-12-10 10:08:57 +0000

    Day 4 Part 2

--- 2023/Day4/Program.cs
+++ 2023/Day4/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-public class Program
+﻿public class Program
 {
     public static void Main()
     {
@@ -18,28 +12,55 @@ public class Program
         string[] lines = File.ReadAllLines(scratchCardData);
 
         var scratchInfos = new List<ScratchInformation>();
+        var copies = new List<Copy>();
 
         foreach (var line in lines)
         {
             var scratchInfo = new ScratchInformation(int.Parse(line.Split(": ").First().Split(' ').Last()));
 
+            // Update total number of Card
+            var copy = copies.FirstOrDefault(x => x.Id == scratchInfo.Id);
+            if (copy != null)
+            {
+                scratchInfo.TotalNumberOfCards += copy.NumberOfCopies;
+            }
+
             var numbers = line.Split(": ").Last().Split(" | ");
 
             var winningNumbers = numbers.First()
-            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Select(x => int.Parse(x));
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x));
 
             var chosenNumbers = numbers.Last()
-            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Select(x => int.Parse(x));
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x));
 
             scratchInfo.WinningNumbers = winningNumbers.Intersect(chosenNumbers);
-            scratchInfo.Worth = (int)Math.Pow(2, scratchInfo.WinningNumbers.Count() - 1);
+
+            if (scratchInfo.WinningNumbers.Any())
+            {
+                scratchInfo.Worth = (int)Math.Pow(2, scratchInfo.WinningNumbers.Count() - 1);
+
+                // Add copy to list of copies
+                for (int i = 1; i <= scratchInfo.WinningNumbers.Count(); i++)
+                {
+                    copy = copies.FirstOrDefault(x => x.Id == scratchInfo.Id + i);
+                    if (copy != null)
+                    {
+                        copy.NumberOfCopies += scratchInfo.TotalNumberOfCards;
+                    }
+                    else
+                    {
+                        copies.Add(new Copy(scratchInfo.Id + i, scratchInfo.TotalNumberOfCards));
+                    }
+                }
+            }
 
             scratchInfos.Add(scratchInfo);
         }
 
         Console.WriteLine($"Part 1 result: {scratchInfos.Sum(x => x.Worth)}");
+        Console.WriteLine($"Part 2 result: {scratchInfos.Sum(x => x.TotalNumberOfCards)}");
     }
 
     public class ScratchInformation
@@ -51,6 +72,19 @@ public class Program
 
         public int Id { get; set; }
         public IEnumerable<int> WinningNumbers { get; set; }
-        public int Worth { get; set; }
+        public int Worth { get; set; } = 0;
+        public int TotalNumberOfCards { get; set; } = 1;
+    }
+
+    public class Copy
+    {
+        public Copy(int id, int numberOfCopies)
+        {
+            Id = id;
+            NumberOfCopies = numberOfCopies;
+        }
+
+        public int Id { get; set; }
+        public int NumberOfCopies { get; set; }
     }
 }
